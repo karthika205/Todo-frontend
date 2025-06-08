e926fa0b14a5d77467db9887dc110ade6a5e8e38
karthika205 <karthi205c@gmail.com>
2025-06-08 20:17:20 +0530

    Add Firebase setup and update dependencies

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
@@ -5,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAUKVASU52pf6uz3XmoNvJz5K6eWFPXu14",
   authDomain: "todo-task-app-a5658.firebaseapp.com",
   projectId: "todo-task-app-a5658",
-  storageBucket: "todo-task-app-a5658.appspot.com", // fix if you had 'storage.app' before
+  storageBucket: "todo-task-app-a5658.appspot.com",
   messagingSenderId: "841757297932",
   appId: "1:841757297932:web:c4eb62d160b6c64c5e8d29",
   measurementId: "G-DJXHE5WFRN"
